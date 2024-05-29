3815441b911f28c146a366b21d49522d33a931a3
esborn13467 <125538862+esborn13467@users.noreply.github.com>
2024-05-29 14:56:03 +0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -4,6 +4,7 @@ import re
 def is_palindrome(s):
     # Normalize the input string
     s = s.lower()  # Convert to lowercase
+    s = re.sub(r'[^a-z0-9]', '', s)  # Remove non-alphanumeric characters
     if s == s[::-1]:
         print("Palindrome")
     else:
